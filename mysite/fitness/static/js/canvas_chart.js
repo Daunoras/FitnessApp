359e8526ea5ba4617cf1f9669ba29b0001b8af20
Justinas Daunoras <justdaunoras@gmail.com>
2024-08-04 05:57:04 +0300

    Changed equadistant x drawint to date proportional. Line draw- chronological by sorting points.

--- mysite/fitness/static/js/canvas_chart.js
+++ mysite/fitness/static/js/canvas_chart.js
@@ -7,20 +7,31 @@ class Chart {
             console.error('error with canvas')
         }
         this.data = data.data;
-        this.labels = data.labels
+        this.labels = data.labels;
     }
 
     drawLineChart() {
         if (!this.ctx) return;
         this.ctx.beginPath();
-        this.ctx.moveTo(10, this.canvas.height - (this.data[0] / Math.max(...this.data)) * this.canvas.height);
-
+        this.ctx.moveTo(0, this.canvas.height - (this.data[0] / Math.max(...this.data)) * this.canvas.height);
+//      date transformation
+        let timestamps = this.labels.map(label => new Date(label).getTime());
+        let minDate = Math.min(...timestamps);
+        let offsetedDates = timestamps.map(date => date - minDate);
+        let dateEndPoint = Math.max(...offsetedDates);
+        let normalizedDates = offsetedDates.map(date => date / dateEndPoint);
+//      coordinate generation
+        let coordinates = [];
         this.data.forEach((value, index) => {
-            let x = 10 + index * (this.canvas.width / this.data.length);
+            let x = this.canvas.width * normalizedDates[index]
             let y = this.canvas.height - (value / Math.max(...this.data)) * this.canvas.height;
-            this.ctx.lineTo(x, y);
+            coordinates.push([x, y]);
         });
-
+        coordinates.sort((a, b) => a[0] - b[0]);
+//      drawing the line
+        coordinates.forEach(coordinate => {
+            this.ctx.lineTo(coordinate[0], coordinate[1]);
+        });
         this.ctx.strokeStyle = 'red';
         this.ctx.stroke();
     }
