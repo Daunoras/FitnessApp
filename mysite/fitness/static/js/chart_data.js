29b16de746e7358b373181a93230645156b4d344
Justinas Daunoras <justdaunoras@gmail.com>
2026-06-03 02:09:07 +0300

    date validation in data display chart

--- mysite/fitness/static/js/chart_data.js
+++ mysite/fitness/static/js/chart_data.js
@@ -1,7 +1,7 @@
 import Chart from './canvas_chart.js';
 
 function fetchDataAndRenderChart(model, startDate, endDate) {
-
+    if (validateDates(startDate, endDate)) {
     const url = `/fitness/api/chart/data/?model=${model}&date_from=${startDate}&date_to=${endDate}`;
     fetch(url)
         .then(response => response.json())
@@ -14,6 +14,21 @@ function fetchDataAndRenderChart(model, startDate, endDate) {
             }
         })
         .catch(error => console.error('Error fetching data:', error));
+    } else {
+        return;
+    }
+}
+
+function validateDates(startDate, endDate) {
+    const warning = document.getElementById('warning');
+
+    if (startDate && endDate && startDate > endDate) {
+        warning.textContent = 'Invalid dates';
+        warning.style.display = "block";
+        return false;
+    }
+    warning.style.display = "none";
+    return true;
 }
 
 let myChart = null;
