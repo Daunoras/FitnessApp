2505527168ee7e59d80496a0cc5f7fead7270b1d
Daunoras <justdaunoras@gmail.com>
2026-07-08 16:17:47 +0300

    homepage dashboard calendar: calendar grid layout

--- mysite/data_charts/static/js/activity_calendar.js
+++ mysite/data_charts/static/js/activity_calendar.js
@@ -9,6 +9,13 @@ class ActivityCalendar {
         for (let day of this.days) {
             let dayCard = document.createElement("div");
             dayCard.textContent = day.date;
+            dayCard.className = 'day';
+            if (day.is_today == true) {
+                dayCard.classList.add('today')
+            }
+            if (day.is_future == true) {
+                dayCard.classList.add('future')
+            }
             this.container.appendChild(dayCard);
         }
     }
